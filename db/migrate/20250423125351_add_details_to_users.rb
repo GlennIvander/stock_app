78a5b77007793288820962e15c9848a820f2ab70
pheebsloyola <pheebsloyola>
2025-05-07 20:29:43 +0800

    Pending for approval has been fixed

--- db/migrate/20250423125351_add_details_to_users.rb
+++ db/migrate/20250423125351_add_details_to_users.rb
@@ -2,6 +2,6 @@ class AddDetailsToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :is_admin, :boolean, default: false
     add_column :users, :balance, :integer
-    add_column :users, :is_pending, :boolean, default: false
+    add_column :users, :is_pending, :boolean, default: true
   end
 end
